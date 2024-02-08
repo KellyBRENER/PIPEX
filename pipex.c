9ab5766a205954951d95df88c5999cb0dd89f1d6
KellyBRENER <kellybrener66@gmail.com>
2024-02-08 11:26:57 +0100

    correction_function

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 14:41:58 by kbrener-          #+#    #+#             */
-/*   Updated: 2024/02/05 16:15:59 by kbrener-         ###   ########.fr       */
+/*   Updated: 2024/02/08 11:21:58 by kbrener-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_getpath(char *cmd, char **env)
 	{
 		if (ft_strnstr(env[i], "PATH=", 5) != NULL)
 		{
-			all_path = ft_split(ft_substr(env[i], 5, strchr(env[i], '\n')), ":"); //extrait les chemin de env[i] apres PATH= jusqu'au "\n"
+			all_path = ft_split(ft_substr(env[i], 5, (size_t)strchr(env[i], '\0')), ':'); //extrait les chemin de env[i] apres PATH= jusqu'au "\n"
 			bool = 1;
 		}
 		i++;
@@ -36,7 +36,7 @@ char	*ft_getpath(char *cmd, char **env)
 	i = 0;
 	while (all_path[i])
 	{
-		path_cmd = ft_strjoin(all_path[i], cmd)
+		path_cmd = ft_strjoin(all_path[i], cmd);
 		if (access(path_cmd, F_OK) == 0)
 		{
 			if(access(path_cmd, X_OK) == 0)
@@ -60,7 +60,7 @@ char	*ft_getpath(char *cmd, char **env)
 //5. integrer le reste des arguments dans le vecteur
 
 //fonction principale qui execute la cmd1 ds le processus enfant et la cmd2 dans le processus parent
-int	pipex(int argc, char **argv, char **env)
+int	pipex(char **argv, char **env)
 {
 	int	fd[2];
 	if (pipe(fd) == -1)
@@ -80,7 +80,7 @@ int	pipex(int argc, char **argv, char **env)
 		int	fd_infile;
 		char	**ve_cmd;
 
-		ve_cmd = split(argv[2], " ");
+		ve_cmd = ft_split(argv[2], ' ');
 		fd_infile = open(argv[1], O_RDONLY);
 		if (fd_infile == -1) {
 			ft_printf("error opening infile");
@@ -91,7 +91,7 @@ int	pipex(int argc, char **argv, char **env)
 		close(fd[0]);
 		close(fd[1]);
 		close(fd_infile);
-		execve(ft_getpath(strjoin("/", ve_cmd[0]), env), ve_cmd, env);
+		execve(ft_getpath(ft_strjoin("/", ve_cmd[0]), env), ve_cmd, env);
 		ft_printf("error executing first command");
 		return -1;
 	}
@@ -99,7 +99,7 @@ int	pipex(int argc, char **argv, char **env)
 	int	fd_outfile;
 	char	**ve_cmd;
 
-	ve_cmd = split(argv[3], " ");
+	ve_cmd = ft_split(argv[3], ' ');
 	fd_outfile = open(argv[4], O_WRONLY);
 	if (fd_outfile == -1)
 	{
@@ -111,7 +111,7 @@ int	pipex(int argc, char **argv, char **env)
 	close(fd[0]);
 	close(fd[1]);
 	close(fd_outfile);
-	execve(ft_getpath(strjoin("/", ve_cmd[0]), env), ve_cmd, env);
+	execve(ft_getpath(ft_strjoin("/", ve_cmd[0]), env), ve_cmd, env);
 	ft_printf("error executing second command");
 	return -1;
 }
@@ -122,7 +122,7 @@ int	main(int argc, char **argv, char **env)
 	{
 		return 4;
 	}
-	if (pipex(argc, argv, env) == -1)
+	if (pipex(argv, env) == -1)
 	{
 		return 3;
 	};
