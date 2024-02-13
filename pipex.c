2167b2c90d0305b6c364c1c51fc96c10b762c8ad
KellyBRENER <kellybrener66@gmail.com>
2024-02-13 12:00:22 +0100

    leak_corrected

--- pipex.c
+++ pipex.c
@@ -6,32 +6,34 @@
 /*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 14:41:58 by kbrener-          #+#    #+#             */
-/*   Updated: 2024/02/13 10:24:31 by kbrener-         ###   ########.fr       */
+/*   Updated: 2024/02/13 11:58:46 by kbrener-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-void	ft_tabfree(char **tab)
+void ft_tabfree(char **tab)
 {
-	int	i;
+	int i;
 
 	i = 0;
-	if (tab) {
-		while (tab[i]) {
+	if (tab)
+	{
+		while (tab[i])
+		{
 			free(tab[i]);
 			i++;
 		}
 	}
 	free(tab);
 }
-//fonction qui recupere le chemin, le PATH, pour chaque commande et l'integre dans le vecteur
-char	*ft_getpath(char *cmd, char **env)
+// fonction qui recupere le chemin, le PATH, pour chaque commande et l'integre dans le vecteur
+char *ft_getpath(char *cmd, char **env)
 {
-	char	**tab_allpath;
-	char	*path_cmd;
-	char	*str_allpath;
-	int	bool;
-	int	i;
+	char **tab_allpath;
+	char *path_cmd;
+	char *str_allpath;
+	int bool;
+	int i;
 
 	i = 0;
 	bool = 0;
@@ -40,19 +42,21 @@ char	*ft_getpath(char *cmd, char **env)
 	path_cmd = NULL;
 	while (env[i] != NULL && bool == 0)
 	{
-		if (ft_strnstr(env[i], "PATH=", 5) != NULL) //cherche path dans env
+		if (ft_strnstr(env[i], "PATH=", 5) != NULL) // cherche path dans env
 		{
 			str_allpath = ft_substr(env[i], 5, (size_t)strchr(env[i], '\0'));
-			tab_allpath = ft_split(str_allpath, ':'); //extrait les chemin de env[i] apres PATH= jusqu'au "\n"
+			tab_allpath = ft_split(str_allpath, ':'); // extrait les chemin de env[i] apres PATH= jusqu'au "\n"
 			free(str_allpath);
 			bool = 1;
 		}
 		i++;
 	}
 	i = 0;
-	while (tab_allpath[i]) {
+	while (tab_allpath[i])
+	{
 		path_cmd = ft_strjoin(tab_allpath[i], cmd);
-		if (access(path_cmd, F_OK | X_OK) == 0) {
+		if (access(path_cmd, F_OK | X_OK) == 0)
+		{
 			ft_tabfree(tab_allpath);
 			return (path_cmd);
 		}
@@ -63,75 +67,87 @@ char	*ft_getpath(char *cmd, char **env)
 	return (NULL);
 }
 
-//fonction qui recupere le vecteur pour chaque commande
-//1. recuperer la commande
-//2. recuperer ce qu'il y a entre "PATH=" et "\n" (l'ensemble des PATH)
-//3. Tester chacun des chemin avec la commande grace a la fonction access
-//4. integrer le chemin trouve dans le vecteur[0]
-//5. integrer le reste des arguments dans le vecteur
+// fonction qui recupere le vecteur pour chaque commande
+// 1. recuperer la commande
+// 2. recuperer ce qu'il y a entre "PATH=" et "\n" (l'ensemble des PATH)
+// 3. Tester chacun des chemin avec la commande grace a la fonction access
+// 4. integrer le chemin trouve dans le vecteur[0]
+// 5. integrer le reste des arguments dans le vecteur
 
-//fonction principale qui execute la cmd1 ds le processus enfant et la cmd2 dans le processus parent
-int	pipex(char **argv, char **env)
+// fonction principale qui execute la cmd1 ds le processus enfant et la cmd2 dans le processus parent
+int pipex(char **argv, char **env)
 {
-	int	fd[2];
-	if (pipe(fd) == -1) {
+	int fd[2];
+	if (pipe(fd) == -1)
+	{
 		perror("zsh");
 		return -1;
 	}
-	int	pid = fork();
-	if (pid == -1) {
+	int pid = fork();
+	if (pid == -1)
+	{
 		perror("zsh");
 		return -1;
 	}
-	if (pid == 0) {
-		int	fd_infile;
-		char	**ve_cmd;
-		char	*cmd_path;
+	if (pid == 0)
+	{
+		int fd_infile;
+		char **ve_cmd;
+		char *cmd_path;
+		char *cmd;
 
-		ve_cmd = ft_split(argv[2], ' ');
 		fd_infile = open(argv[1], O_RDONLY);
-		if (fd_infile == -1) {
+		if (fd_infile == -1)
+		{
 			perror("zsh");
 			return -1;
 		}
+		ve_cmd = ft_split(argv[2], ' ');
+		cmd = ft_strjoin("/", ve_cmd[0]);
 		dup2(fd_infile, STDIN_FILENO);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[0]);
 		close(fd[1]);
 		close(fd_infile);
-		cmd_path = ft_getpath(ft_strjoin("/", ve_cmd[0]), env);
+		cmd_path = ft_getpath(cmd, env);
 		execve(cmd_path, ve_cmd, env);
 		free(cmd_path);
+		free(cmd);
 		ft_tabfree(ve_cmd);
 		perror("zsh");
 		return -1;
 	}
-	int	fd_outfile;
-	char	**ve_cmd;
-	char	*cmd_path;
+	int fd_outfile;
+	char **ve_cmd;
+	char *cmd_path;
+	char *cmd;
 
-	ve_cmd = ft_split(argv[3], ' ');
 	fd_outfile = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0777);
-	if (fd_outfile == -1) {
+	if (fd_outfile == -1)
+	{
 		perror("zsh");
 		return -1;
 	}
+	ve_cmd = ft_split(argv[3], ' ');
+	cmd = ft_strjoin("/", ve_cmd[0]);
 	dup2(fd[0], STDIN_FILENO);
 	dup2(fd_outfile, STDOUT_FILENO);
 	close(fd[0]);
 	close(fd[1]);
 	close(fd_outfile);
-	cmd_path = ft_getpath(ft_strjoin("/", ve_cmd[0]), env);
+	cmd_path = ft_getpath(cmd, env);
 	execve(cmd_path, ve_cmd, env);
 	free(cmd_path);
+	free(cmd);
 	ft_tabfree(ve_cmd);
 	perror("zsh");
 	return -1;
 }
 
-int	main(int argc, char **argv, char **env)
+int main(int argc, char **argv, char **env)
 {
-	if (argc != 5) {
+	if (argc != 5)
+	{
 		write(1, "incorrect argument count", 24);
 		return 1;
 	}
